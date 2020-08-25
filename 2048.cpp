915a6d625c8c8098747cdadcc9c317ab2cd52ad8
Chuong Cao <62738863+Flares243@users.noreply.github.com>
2020-08-25 15:29:30 +0700

    Update 2048.cpp

--- 2048.cpp
+++ 2048.cpp
@@ -1,86 +1,59 @@
 #include "D:\2048.h"
 
-using namespace std;
-
-
-
 int main()
-
 {
 	srand(time(0));
 	int a[4][4] ;
 	int d = 4, c = 4;
-	char selection;
-
-
-
+	char c;
 	cout << "\n WELCOM TO GAME 2048";
-
 	cout << "\n======================";
-
-
 	cout << "\n A - Move Left";
-
 	cout << "\n D - Move Right";
-	
 	cout << "\n W - Move Up";
-
 	cout << "\n S - Move Down";
-
 	cout << "\n X - Exit";
+	
+	while(1) {
+		cout << "\n Enter selection: ";
+		cin >> c;
 
-	cout << "\n Enter selection: ";
+		switch (c)
+		{
+			case 'A':
+			case 'a': {
 
-	// read the input
+				break;	
+			}
 
-	cin >> selection;
+			case 'D':
+			case 'd': {
 
+				break;	
+			}
 
+			case 'S':
+			case 's': {
 
-	switch (selection)
+				break;	
+			}
 
-	{
+			case 'W':
+			case 'w': {
 
-	case 'A':
+				break;	
+			}
 
-	case 'a': 
-
+			case 'X':
+			case 'x': {
+				exit(0);
 				break;
-	
-	case 'D':
+			}
 
-	case 'd':
+			default: printf("Khong co nut %c", c) ;
 
-				break;
-	
-	case 'S' :
-	
-	case 's' :
-				break;
-
-	
-	case 'W':
-
-	case 'w': 
-
-			  break;
-
-	case 'X':
-
-	case 'x': 
-
-			  break;
-
-
-	default: cout << "" ;
-
-
+		}
 	}
 
-	cout << "\n";
-
-
-
 	return 0;
-
 }
