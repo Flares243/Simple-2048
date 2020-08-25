601d7af339a1f8a869100f6eecc88bff8bf7229d
kakaomany <nguyenhanghai941412001@gmail.com>
2020-08-25 15:01:22 +0700

    Create 2048.cpp

---
+++ 2048.cpp
@@ -0,0 +1,86 @@
+#include "D:\2048.h"
+
+using namespace std;
+
+
+
+int main()
+
+{
+	srand(time(0));
+	int a[4][4] ;
+	int d = 4, c = 4;
+	char selection;
+
+
+
+	cout << "\n WELCOM TO GAME 2048";
+
+	cout << "\n======================";
+
+
+	cout << "\n A - Move Left";
+
+	cout << "\n D - Move Right";
+	
+	cout << "\n W - Move Up";
+
+	cout << "\n S - Move Down";
+
+	cout << "\n X - Exit";
+
+	cout << "\n Enter selection: ";
+
+	// read the input
+
+	cin >> selection;
+
+
+
+	switch (selection)
+
+	{
+
+	case 'A':
+
+	case 'a': 
+
+				break;
+	
+	case 'D':
+
+	case 'd':
+
+				break;
+	
+	case 'S' :
+	
+	case 's' :
+				break;
+
+	
+	case 'W':
+
+	case 'w': 
+
+			  break;
+
+	case 'X':
+
+	case 'x': 
+
+			  break;
+
+
+	default: cout << "" ;
+
+
+	}
+
+	cout << "\n";
+
+
+
+	return 0;
+
+}
